93502079abde935c469dd770ce7c7f8e1a793206
brock810 <130688727+brock810@users.noreply.github.com>
2023-04-28 15:55:43 -0500

    another file

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var vehicle = new car();
+            vehicle.Make = "Mustang";
+            vehicle.Model = "GT";
+            vehicle.Year = 2008;
+
+            Console.WriteLine($"wow that is a nice {vehicle.Make}, its a {vehicle.Model}? is the year of that {vehicle.Year}?");
+            
         }
     }
 }
+
